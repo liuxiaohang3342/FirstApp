c484ec5fccde4136140c9216bfbda4aa2ab8bbf1
liuxiaohang3342 <liuxiaohang3342@gmail.com>
2018-09-13 23:52:59 +0800

    天气页改版

--- app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherAdapter.java
+++ app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherAdapter.java
@@ -1,10 +1,16 @@
 package com.example.lxh.firstapp.home.weather;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.lxh.firstapp.R;
 import com.example.lxh.firstapp.bean.WeatherDayInfo;
 
 import java.util.List;
@@ -13,13 +19,43 @@ import java.util.List;
  * Created by lxh on 2018/8/3.
  */
 
-public class WeatherAdapter extends BaseQuickAdapter<WeatherDayInfo, BaseViewHolder> {
+public class WeatherAdapter extends PagerAdapter {
 
-    public WeatherAdapter(@LayoutRes int layoutResId, @Nullable List<WeatherDayInfo> data) {
-        super(layoutResId, data);
+
+    private Context mContext;
+    private List<WeatherDayInfo> mList;
+
+    public WeatherAdapter(Context context, List<WeatherDayInfo> mList) {
+        mContext = context;
+        this.mList = mList;
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, WeatherDayInfo item) {
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.weather_item_layout, container, false);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (object instanceof View) {
+            container.removeView((View) object);
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mList.get(position).getDate();
     }
 }
--- app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherFragment.java
+++ app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherFragment.java
@@ -1,14 +1,13 @@
 package com.example.lxh.firstapp.home.weather;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.lxh.firstapp.R;
 import com.example.lxh.firstapp.base.core.fragment.BaseMVPFragment;
-import com.example.lxh.firstapp.bean.WeatherDayInfo;
 import com.example.lxh.firstapp.bean.WeatherInfo;
 
 /**
@@ -17,12 +16,9 @@ import com.example.lxh.firstapp.bean.WeatherInfo;
 
 public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherView> implements IWeatherView {
 
-    private RecyclerView mRecyclerView;
     private TextView mWenduView;
-    private TextView mPmView;
-    private TextView mQualityView;
-    private TextView mWindView;
-    private TextView mTipView;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected WeatherPresenter createPresenter() {
@@ -38,12 +34,9 @@ public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherV
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mWenduView = (TextView) view.findViewById(R.id.tv_wendu);
-        mQualityView = (TextView) view.findViewById(R.id.tv_quality);
-        mPmView = (TextView) view.findViewById(R.id.tv_pm);
-        mWindView = (TextView) view.findViewById(R.id.tv_wind);
-        mTipView = (TextView) view.findViewById(R.id.tv_desc);
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.rlv_weather);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mTabLayout = (TabLayout) view.findViewById(R.id.tl_weather);
+        mViewPager = (ViewPager) view.findViewById(R.id.vp_weather);
+        mTabLayout.setupWithViewPager(mViewPager);
         showLoadingView();
         getPresenter().requestData();
     }
@@ -59,11 +52,6 @@ public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherV
     public void onRequestSuccess(WeatherInfo weatherInfo) {
         showContentView();
         mWenduView.setText(weatherInfo.getWendu());
-        mQualityView.setText(weatherInfo.getQuality());
-        mPmView.setText("PM:" + String.valueOf(weatherInfo.getPm25()));
-        WeatherDayInfo todayInfo = weatherInfo.getForecast().get(0);
-        mWindView.setText(todayInfo.getFx() + "   " + todayInfo.getFl() + "    湿度    " + weatherInfo.getShidu());
-        mTipView.setText(weatherInfo.getGanmao());
-        mRecyclerView.setAdapter(new WeatherAdapter(R.layout.home_weather_item_layout, weatherInfo.getForecast()));
+        mViewPager.setAdapter(new WeatherAdapter(getActivity(), weatherInfo.getForecast()));
     }
 }

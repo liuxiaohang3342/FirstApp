e372e7971916ea724a46b435b01e0356aee2010b
liuxiaohang3342 <173050594@qq.com>
2018-09-14 11:19:58 +0800

    天气页面的完成

--- app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherAdapter.java
+++ app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherAdapter.java
@@ -1,15 +1,17 @@
 package com.example.lxh.firstapp.home.weather;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.lxh.firstapp.R;
 import com.example.lxh.firstapp.bean.WeatherDayInfo;
 
@@ -21,6 +23,8 @@ import java.util.List;
 
 public class WeatherAdapter extends PagerAdapter {
 
+    private ColorMatrix mMatrix;
+    private int mColor;
 
     private Context mContext;
     private List<WeatherDayInfo> mList;
@@ -28,6 +32,7 @@ public class WeatherAdapter extends PagerAdapter {
     public WeatherAdapter(Context context, List<WeatherDayInfo> mList) {
         mContext = context;
         this.mList = mList;
+        mColor = context.getColor(R.color.colorTheme);
     }
 
     @Override
@@ -43,19 +48,81 @@ public class WeatherAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.weather_item_layout, container, false);
+        initView(view, position);
         container.addView(view);
         return view;
     }
 
+    private void initView(View view, int position) {
+        WeatherDayInfo info = mList.get(position);
+        TextView wendu = (TextView) view.findViewById(R.id.tv_wendu);
+        TextView notice = (TextView) view.findViewById(R.id.tv_notice);
+        TextView sunrise = (TextView) view.findViewById(R.id.tv_sunrise);
+        TextView sunset = (TextView) view.findViewById(R.id.tv_sunset);
+        TextView qiya = (TextView) view.findViewById(R.id.tv_qiya);
+        TextView fengx = (TextView) view.findViewById(R.id.tv_fengx);
+        TextView fengji = (TextView) view.findViewById(R.id.tv_fengji);
+        ImageView weather = (ImageView) view.findViewById(R.id.iv_weather);
+        String low = info.getLow().substring(3);
+        String high = info.getHigh().substring(3);
+        wendu.setText(String.format(mContext.getString(R.string.weather_wendu), info.getType(), low, high));
+        notice.setText(info.getNotice());
+        sunrise.setText(info.getSunrise());
+        sunset.setText(info.getSunset());
+        qiya.setText(String.valueOf(info.getAqi()));
+        fengx.setText(info.getFx());
+        fengji.setText(info.getFl());
+        setWeatherType(weather, info.getType());
+    }
+
+    public ColorMatrixColorFilter getColorFilter() {
+        if (mMatrix == null) {
+            mMatrix = new ColorMatrix();
+        }
+        mMatrix.set(new float[]{
+                0, 0, 0, 0, Color.red(mColor),
+                0, 0, 0, 0, Color.green(mColor),
+                0, 0, 0, 0, Color.blue(mColor),
+                0, 0, 0, 1, 0
+        });
+        return new ColorMatrixColorFilter(mMatrix);
+    }
+
+    private Drawable getDrawableWithType(String type) {
+        if (type.contains("沙")) {
+            return mContext.getDrawable(R.drawable.sand);
+        }
+        if (type.contains("雾")) {
+            return mContext.getDrawable(R.drawable.fog);
+        }
+        if (type.contains("冰")) {
+            return mContext.getDrawable(R.drawable.icecream);
+        }
+        if (type.contains("雪")) {
+            return mContext.getDrawable(R.drawable.snow);
+        }
+        if (type.contains("雨")) {
+            return mContext.getDrawable(R.drawable.bigest_rain);
+        }
+        if (type.contains("云")) {
+            return mContext.getDrawable(R.drawable.cloudy);
+        }
+        return mContext.getDrawable(R.drawable.sunny);
+    }
+
+    private void setWeatherType(ImageView weather, String type) {
+        Drawable drawable = getDrawableWithType(type);
+        drawable.getCurrent().setColorFilter(getColorFilter());
+        weather.setBackground(drawable);
+    }
+
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        if (object instanceof View) {
-            container.removeView((View) object);
-        }
+        container.removeView((View) object);
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mList.get(position).getDate();
+        return mList.get(position).getDate().substring(3);
     }
 }
--- app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherFragment.java
+++ app/src/main/java/com/example/lxh/firstapp/home/weather/WeatherFragment.java
@@ -5,10 +5,13 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lxh.firstapp.R;
 import com.example.lxh.firstapp.base.core.fragment.BaseMVPFragment;
 import com.example.lxh.firstapp.bean.WeatherInfo;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by lxh on 2018/8/3.
@@ -19,6 +22,10 @@ public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherV
     private TextView mWenduView;
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+
+    private TextView mCityView;
+    private TextView mShiduView;
+    private TextView mQualityView;
 
     @Override
     protected WeatherPresenter createPresenter() {
@@ -34,11 +41,25 @@ public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherV
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mWenduView = (TextView) view.findViewById(R.id.tv_wendu);
+        mCityView = (TextView) view.findViewById(R.id.tv_city);
+        mCityView.setOnClickListener(this);
+        mShiduView = (TextView) view.findViewById(R.id.tv_shidu);
+        mQualityView = (TextView) view.findViewById(R.id.tv_quality);
         mTabLayout = (TabLayout) view.findViewById(R.id.tl_weather);
         mViewPager = (ViewPager) view.findViewById(R.id.vp_weather);
         mTabLayout.setupWithViewPager(mViewPager);
         showLoadingView();
         getPresenter().requestData();
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.tv_city:
+                Toast.makeText(getContext(), "切换城市", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     @Override
@@ -52,6 +73,8 @@ public class WeatherFragment extends BaseMVPFragment<WeatherPresenter, IWeatherV
     public void onRequestSuccess(WeatherInfo weatherInfo) {
         showContentView();
         mWenduView.setText(weatherInfo.getWendu());
+        mShiduView.setText(String.format(getString(R.string.weather_shidu_pm), weatherInfo.getShidu(), weatherInfo.getPm25()));
+        mQualityView.setText(weatherInfo.getQuality());
         mViewPager.setAdapter(new WeatherAdapter(getActivity(), weatherInfo.getForecast()));
     }
 }
